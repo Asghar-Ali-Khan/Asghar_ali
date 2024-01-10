5cd97400a77fd912a9beb62db455896331a7a341
Asghar Ali <asgharalikhan5k@gmail.com>
2024-01-10 01:18:24 -0800

    Add product's deatails

--- Products_management/resources/views/dashboard.blade.php
+++ Products_management/resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
 @include('header');
 <div class="con" style=" display: grid;
-grid-template-columns: repeat(6, 1fr); 
+grid-template-columns: repeat(8, 1fr); 
 gap:35px; 
 padding:60px;">
 @foreach ($images as $img )
-
 <div class="card" style="width: 18rem;">
     <img src="images/{{$img->images}}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">{{$img->name}}</h5>
-      <h5 class="card-title">{{$img->price}}</h5>
+      <h5 class="card-title">Rs:{{$img->price}}</h5>
       <p class="card-text">{{$img->description}}</p>
       <a href="#" class="btn btn-primary">Add To Card</a>
     </div>
--- Products_management/routes/web.php
+++ Products_management/routes/web.php
@@ -2,13 +2,12 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-Route::get('/',[ProductController::class,'show'])->name('/products');
+Route::get('/products',[ProductController::class,'show'])->name('/products');
 Route::controller(ProductController::class)->group(function(){
     Route::get('images/{id}/edit','edit');
     Route::get('products/{id}/delete','delete');
     Route::get('/all','index')->name('/all');
     Route::put('products/{id}/update','update');
 Route::post('/add','store')->name('save');
-Route::get('/dash','dash')->name('dashboard');
-
+Route::get('/','dash')->name('dashboard');
 });
