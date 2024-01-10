1f12d1d8c965203888dceb00f2edee1471b6a0e5
Asghar Ali <asgharalikhan5k@gmail.com>
2024-01-10 00:43:19 -0800

    change button\

--- Products_management/resources/views/dashboard.blade.php
+++ Products_management/resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@ padding:60px;">
       <h5 class="card-title">{{$img->name}}</h5>
       <h5 class="card-title">{{$img->price}}</h5>
       <p class="card-text">{{$img->description}}</p>
-      <a href="#" class="btn btn-primary">Buy Now</a>
+      <a href="#" class="btn btn-primary">Add To Card</a>
     </div>
   </div>
   <br><br><br>
