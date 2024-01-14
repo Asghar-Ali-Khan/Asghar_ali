b1b75d936d373a0674fb5ac582079b70f85fd5ba
Asghar Ali <asgharalikhan5k@gmail.com>
2024-01-14 01:57:32 -0800

    make it more responsible

--- Products_management/resources/views/dashboard.blade.php
+++ Products_management/resources/views/dashboard.blade.php
@@ -1,13 +1,12 @@
-@include('header');
+@include('header')
 <link rel="stylesheet" href="style.css">
 <div class="con">
 @foreach ($images as $img )
-<div class="card" style="width: 18rem;">
-    <img src="images/{{$img->images}}" class="card-img-top" alt="...">
+<div class="card mx-auto mt-5 " style="width: 20rem; height: -webkit-fill-available;">
+    <img src="images/{{$img->images}}" class="card-img-top" height="200px" width="400px">
     <div class="card-body">
       <h5 class="card-title">{{$img->name}}</h5>
       <h5 class="card-title">Rs:{{$img->price}}</h5>
-      <p class="card-text">{{$img->description}}</p>
       <a href="#" class="btn btn-primary">Add To Card</a>
     </div>
   </div>
--- Products_management/resources/views/header.blade.php
+++ Products_management/resources/views/header.blade.php
@@ -23,14 +23,5 @@
         </ul>
     </div>
 </nav>
-
-<div class="container mt-5">
-    <!-- Your content goes here -->
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 </body>
 </html>
